e74329fee4490fdd01ce8299359d90cd431edb0d
alvescy95 <changyuen95@gmail.com>
2025-10-15 01:47:43 +0800

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer_area_two">
     <div class="container">
         <div class="row footer_top">
-            <div class="col-lg-4 col-sm-7 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="col-lg-6 col-sm-7 wow fadeInUp" data-wow-delay="0.2s">
                 <div class="f_widget info_widget">
                     <a href="#" class="f_logo"><img src="img/Logo.png" alt=""></a>
                     <div class="info_list">
@@ -22,38 +22,22 @@
                             <p><a href="mailto:jay@prislight.net">jay@prislight.net</a></p>
                         </div>
                     </div>
-                    <div class="social_icon">
-                        <a href="#"><i class="ti-facebook"></i></a>
-                        <a href="#"><i class="ti-pinterest"></i></a>
-                        <a href="#"><i class="ti-twitter-alt"></i></a>
-                        <a href="#"><i class="ti-vimeo-alt"></i></a>
-                        <a href="#"><i class="ti-dribbble"></i></a>
-                    </div>
+                  
                 </div>
             </div>
-            <div class="col-lg-3 col-sm-5 wow fadeInUp" data-wow-delay="0.4s">
-                <div class="f_widget link_widget">
-                    <h2 class="f_title">Usefull Links</h2>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Support Center (Faq)</a></li>
-                        <li><a href="#">Features Overview</a></li>
-                        <li><a href="#">Welcome Page</a></li>
-                        <li><a href="#">Under Maintenance</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-5 wow fadeInUp" data-wow-delay="0.6s">
+          
+            <div class="col-lg-3 col-sm-5 wow fadeInUp" data-wow-delay="0.6s">
                 <div class="f_widget link_widget">
                     <h2 class="f_title">Navigation</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/aboutus">About Us</a></li>
+                        <li><a href="/team">Partner</a></li>
+                        <li><a href="/portfolio">Portfolio</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-3 col-sm-7 wow fadeInUp" data-wow-delay="0.8s">
+            <div class="col-lg-3 col-sm-12 wow fadeInUp" data-wow-delay="0.8s">
                 <div class="f_widget subscribe_widget">
                     <h2 class="f_title">Subscribe to Our Newsletter</h2>
                     <p>We are the leaders in the construction and building industries.</p>
